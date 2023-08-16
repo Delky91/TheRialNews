d7914dbc4223a1fc10058ad02e56acffc94769c5
Luis Miño B <l.mino.bustos@gmail.com>
2023-08-16 16:35:57 -0400

    Feat scaffold publicaciones, comentarios

---
+++ db/migrate/20230816203449_create_publicacions.rb
@@ -0,0 +1,11 @@
+class CreatePublicacions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :publicacions do |t|
+      t.string :title
+      t.text :content
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
